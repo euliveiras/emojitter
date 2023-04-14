01d30b49fd824b1675a4f675fa25679519196d0b
euliveiras <matheus.skm@hotmail.com>
2023-04-14 15:11:10 -0300

    adding post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,22 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState<string>("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -27,7 +38,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis"
         className="grow bg-transparent outline-none"
+        value={input}
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>create</button>
     </div>
   );
 };
@@ -70,7 +86,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {[...data, ...data, ...data].map((data) => (
+      {data.map((data) => (
         <PostView {...data} key={data.post.id} />
       ))}
     </div>
