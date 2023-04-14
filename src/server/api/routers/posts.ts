064ec0de3b7092ee34c73050726ee5f258bb50dc
euliveiras <matheus.skm@hotmail.com>
2023-04-14 19:06:59 -0300

    routing

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,4 @@
-import { User } from "@clerk/nextjs/dist/api";
+import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
